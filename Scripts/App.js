8057909f10d2b1a1044f089e4b8d488491e89c7f
Aslanshrestha <149399704+Aslanshrestha@users.noreply.github.com>
2023-12-13 08:54:00 -0700

    still working

--- Scripts/App.js
+++ Scripts/App.js
@@ -9,20 +9,31 @@ The elements must come to gether to resemble an image.  Your Greeting Card
 must also include text, an image, and music. */
 
 var background=document.createElement("div");
-var text=document.createElement("div");
+var text =document.createElement("div");
 var triangleOne=document.createElement("div");
 var triangleTwo=document.createElement("div");
 var triangleThree=document.createElement("div");
 
 background.style.width= "98%";
-background.style.backgroundColor= "#b57f3e";
+background.style.backgroundColor= "red";
 background.style.padding= "10px";
 document.body.append(background); 
- 
-background.text
 
-text.style.innerHTML= "Merry Christmas"
-text.style.backgroundColor= "transparent"
-text.style.textAlign= "center"
-text.style.color="maroon"
-text.style.fontFamily="cursive"
+background.append(text);
+background.append(triangleOne);
+
+text.style.innerHTML= "Merry Christmas";
+text.style.backgroundColor= "green";
+text.style.textAlign= "center";
+text.style.color="maroon";
+text.style.fontFamily="cursive";
+
+triangleOne.style.border = "green solid 50px"; 
+triangleOne.style.width = "0px"; 
+triangleOne.style.height = "0px"; 
+triangleOne.style.textAlign= "center";
+triangleOne.style.borderLeftColor = "transparent"; 
+triangleOne.style.borderRightColor = "transparent"; 
+triangleOne.style.borderTopColor = "transparent"; 
+triangleOne.style.borderBottomWidth = "200px" 
+document.body.append(triangleOne); 
